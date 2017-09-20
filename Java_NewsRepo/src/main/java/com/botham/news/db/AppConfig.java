1fdc9cdfad6b674f53d93e2ca5cd6bd71215fa02
steve <steve@fncserver3>
2017-09-20 15:26:53 -0400

    1

---
+++ Java_NewsRepo/src/main/java/com/botham/news/db/AppConfig.java
@@ -0,0 +1,9 @@
+package com.botham.news.db;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.botham"})
+public class AppConfig {
+}
