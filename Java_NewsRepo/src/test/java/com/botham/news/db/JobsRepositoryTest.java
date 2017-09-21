0a92437cca3a67e3e56f6ad4fa7bf004c4e2efa8
steve <steve@fncserver3>
2017-09-20 22:02:49 -0400

    1

--- Java_NewsRepo/src/test/java/com/botham/news/db/JobsRepositoryTest.java
+++ Java_NewsRepo/src/test/java/com/botham/news/db/JobsRepositoryTest.java
@@ -1,25 +1,8 @@
 package com.botham.news.db;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {AppConfig.class})
 public class JobsRepositoryTest {
 
-    @Autowired
-    private JobsRepository jobsRepository;
- 
-    // write test cases here
-    
-    @Test
-    public void whenFindByName_thenReturnJob() {
-    	//jobsRepository.findByName("a");
-    	jobsRepository.findAll();
-    }
+
     
  
 }
