734392f6984e7747592ad666d3830f4acc8e195d
steve <steve@fncserver3>
2017-09-20 22:20:31 -0400

    1

--- Java_Base/src/com/botham/base/constants/GlobalConstants.java
+++ Java_Base/src/com/botham/base/constants/GlobalConstants.java
@@ -4,4 +4,6 @@ public class GlobalConstants {
 
 		public static final String ROW_DELIM = "|";
 		public static final String FIELD_DELIM = ",";
+		
+		public static final String ANOTHER = "Another";
 }
