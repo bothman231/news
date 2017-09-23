f466bfa67fbf565739d59408a3a3c6abf83829b5
steve <steve@fncserver3>
2017-09-22 20:28:15 -0400

    1

--- Java_Base/src/main/java/com/botham/base/GlobalConstants.java
+++ Java_Base/src/main/java/com/botham/base/GlobalConstants.java
@@ -1,4 +1,4 @@
-package com.botham.base.constants;
+package com.botham.base;
 
 public class GlobalConstants {
 	
---
+++ Java_Base/src/test/java/com/botham/base/test/GlobalConstantsTest.java
@@ -0,0 +1,24 @@
+package com.botham.base.test;
+
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.botham.base.GlobalConstants;
+
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.Test;
+
+@RunWith(SpringRunner.class)
+public class GlobalConstantsTest {
+	Logger log = LoggerFactory.getLogger(GlobalConstantsTest.class);
+
+	@Test
+	public void GlobalConstantsEchoTest() {
+		if (log.isDebugEnabled()) {
+			log.debug(GlobalConstants.STARTS);
+		}
+	}
+
+}
