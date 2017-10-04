94d395a9332cd9663578ee52e32d8f6894d5f8f1
steve <steve@fncserver3>
2017-10-04 15:11:07 -0400

    1

---
+++ Java_NewsRestApp1/src/main/java/com/botham/news/rest/Java_NewsRestApp1/JavaNewsRestApp1Application.java
@@ -0,0 +1,12 @@
+package com.botham.news.rest.Java_NewsRestApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNewsRestApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNewsRestApp1Application.class, args);
+	}
+}
