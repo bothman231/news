ddb977dcf679bc9f6f063a821eeb6e169b2f8e5f
steve <steve@fncserver3>
2017-10-05 16:32:38 -0400

    1

--- Java_NewsRestApp1/src/main/java/com/botham/news/MyRestController.java
+++ Java_NewsRestApp1/src/main/java/com/botham/news/MyRestController.java
@@ -91,9 +91,7 @@ import com.botham.news.rest.Thing;
 				   
 		return responseEntity1;
 	   }
-	   
-	  	   
-	   
+	   	   
 	   @Consumes({"*", "application/xml", "application/json", "text/plain,text/html"})
 	   @Produces({"application/xml", "application/json", "text/plain,text/html"})
 	   @RequestMapping("/t")
