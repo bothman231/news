2e01d91cd2cb01fb6aee8c9a347a4197deb8beee
steve <steve@fncserver3>
2017-10-05 15:14:37 -0400

    1

--- Java_NewsRestApp1/src/main/java/com/botham/news/MyRestController.java
+++ Java_NewsRestApp1/src/main/java/com/botham/news/MyRestController.java
@@ -89,8 +89,7 @@ import com.botham.news.rest.Thing;
 		
 		ResponseEntity<RestEntity> responseEntity1 = new ResponseEntity<RestEntity>(re, HttpStatus.OK);
 
-		
-		   
+				   
 		return responseEntity1;
 	   }
 	   
