fb75e9b464f375621dfd704afff936714de92a83
FNCSERVER3\steve <sbotham1968@gmail.com>
2017-11-06 00:38:03 -0500

    force build

--- Java_Scheduler/src/main/java/com/botham/Application.java
+++ Java_Scheduler/src/main/java/com/botham/Application.java
@@ -7,9 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 import com.botham.db.resource.ResourceRepository;
-
 
 @SpringBootApplication
 @EnableScheduling
